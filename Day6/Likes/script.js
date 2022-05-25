a3f0eded0efc7247da8d0a2507fec86e1289b78c
Sherline Au <au.sherline@gmail.com>
2022-05-24 19:01:32 -0500

    likes core assignment

---
+++ Day6/Likes/script.js
@@ -0,0 +1,11 @@
+function postCounter1() {
+    document.querySelector("#number1").innerText++;
+}
+
+function postCounter2() {
+    document.querySelector("#number2").innerText++;
+}
+
+function postCounter3() {
+    document.querySelector("#number3").innerText++;
+}
